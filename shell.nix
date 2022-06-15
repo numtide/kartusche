e0b058f5e13514bfff2a64113973e43a6aeb1297
Dragan Milic <dragan@netice9.com>
2022-06-15 16:56:56 +0100

    added shell.nix

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let pkgs = import <nixpkgs> { }; in
+pkgs.mkShell {
+  packages = [
+    pkgs.elmPackages.elm
+    pkgs.gcc
+    pkgs.go_1_18
+    pkgs.nodejs-16_x
+    pkgs.gotools
+    pkgs.gopls
+    pkgs.go-outline
+    pkgs.gocode
+    pkgs.gopkgs
+    pkgs.gocode-gomod
+    pkgs.godef
+    pkgs.golint
+  ];
+}
