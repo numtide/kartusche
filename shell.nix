4d803c902621c07cf80c623c6688ab497cffc565
Dragan Milic <dragan@netice9.com>
2023-02-02 23:05:46 +0000

    revert old shell.nix

--- shell.nix
+++ shell.nix
@@ -1,22 +1,20 @@
-let
-  unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
-in
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (import ./nix/sources.nix).nixpkgs { } }:
 pkgs.mkShell {
   packages = [
     pkgs.gcc
-    unstable.go_1_19
-    unstable.gotools
-    unstable.gopls
-    unstable.go-outline
-    unstable.gocode
-    unstable.gopkgs
-    unstable.gocode-gomod
-    unstable.godef
-    unstable.golint
-    unstable.gh
-    unstable.delve
-    unstable.go-tools
+    pkgs.go_1_19
+    pkgs.gotools
+    pkgs.gopls
+    pkgs.go-outline
+    pkgs.gocode
+    pkgs.gopkgs
+    pkgs.gocode-gomod
+    pkgs.godef
+    pkgs.golint
+    pkgs.gh
+    pkgs.delve
+    pkgs.go-tools
+    pkgs.nixpkgs-fmt
   ];
-  hardeningDisable = [ "all" ];  # to build the cross-compiler
 }
+
